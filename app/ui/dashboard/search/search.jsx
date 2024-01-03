18d62620e56f8d852dee5d125747b73c5275dfa3
meshachvijaya <meshachvijaya@gmail.com>
2024-01-03 16:33:01 +0700

    apply search bar change

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -1,11 +1,33 @@
+"use client";
+
 import { MdSearch } from "react-icons/md";
 import styles from "./search.module.css";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const Search = ({ placeholder }) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathName = usePathname();
+
+  const handleSearch = (e) => {
+    const params = new URLSearchParams(searchParams);
+
+    params.set("q", e.target.value);
+
+    replace(`${pathName}?${params}`);
+  };
+
+  console.log(searchParams);
+  console.log(pathName);
   return (
     <div className={styles.container}>
       <MdSearch />
-      <input type="text" placeholder={placeholder} className={styles.input} />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={styles.input}
+        onChange={handleSearch}
+      />
     </div>
   );
 };
