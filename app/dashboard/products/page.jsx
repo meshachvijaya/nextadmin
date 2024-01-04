753a7fd49e83a6a543d2145cf50de5dbe794dc7e
meshachvijaya <meshachvijaya@gmail.com>
2024-01-04 15:29:09 +0700

    update product dashboard

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -3,8 +3,15 @@ import Link from "next/link";
 import styles from "@/app/ui/dashboard/products/products.module.css";
 import Search from "@/app/ui/dashboard/search/search";
 import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import { useSearchParams } from "next/navigation";
+import { fetchProducts } from "@/app/lib/data";
 
-const ProductsPage = () => {
+const ProductsPage = async () => {
+  const q = searchParams?.q || "";
+  const page = searchParams?.page || 1;
+
+  const { count, products } = await fetchProducts(q, page);
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -25,36 +32,38 @@ const ProductsPage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.product}>
-                <Image
-                  src="/noproduct.jpg"
-                  alt=""
-                  width={40}
-                  height={40}
-                  className={styles.productImage}
-                />
-                iPhone 14
-              </div>
-            </td>
-            <td>Best phone</td>
-            <td>Rp. 12.000.000</td>
-            <td>12 Dec 2023</td>
-            <td>50</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href="/dashboard/products/test">
-                  <button className={`${styles.button} ${styles.view}`}>
-                    View
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>
+                <div className={styles.product}>
+                  <Image
+                    src={product.img || "/noproduct.jpg"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.productImage}
+                  />
+                  {product.title}
+                </div>
+              </td>
+              <td>{product.description}</td>
+              <td>Rp. {product.price}</td>
+              <td>{product.createdAt.toString().slice(4, 16)}</td>
+              <td>{product.stock}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/products/test${product.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
                   </button>
-                </Link>
-                <button className={`${styles.button} ${styles.delete}`}>
-                  Delete
-                </button>
-              </div>
-            </td>
-          </tr>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <Pagination />
--- app/lib/data.js
+++ app/lib/data.js
@@ -10,10 +10,31 @@ export const fetchUsers = async (q, page) => {
   try {
     connectToDB();
     const count = await User.find({ username: { $regex: regex } }).count();
-    const users = await User.find({ username: { $regex: regex } }).limit(ITEM_PER_PAGE).skip(ITEM_PER_PAGE * (page-1));
-  return {count, users};
+    const users = await User.find({ username: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    return { count, users };
   } catch (err) {
     console.log(err);
     throw new Error("Failed to fetch Users!");
+  }
+};
+
+export const fetchProducts = async (q, page) => {
+  // console.log(q);
+  const regex = new RegExp(q, "i");
+
+  const ITEM_PER_PAGE = 3;
+
+  try {
+    connectToDB();
+    const count = await Product.find({ title: { $regex: regex } }).count();
+    const products = await Product.find({ title: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    return { count, products };
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch Products!");
   }
 };
